66ae46f8036284140ada3811b7a8bd255964ad45
rinat <astrakhan.rinat@gmail.com>
2022-08-03 13:41:33 +0300

    9.1. String [#237407] v2

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -3,7 +3,6 @@ package ru.job4j.condition;
 public class Greeting {
 
     public static void main(String[] args) {
-
         String idea = "I like Java!";
         System.out.println(idea);
         idea += " But I am a newbie. ";
